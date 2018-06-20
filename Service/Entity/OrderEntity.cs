4cceed67ccf5447c8c1a42b35b039fa554a3bce9
vz深渊 <exceed295@hotmail.com>
2018-06-21 03:55:23 +0800

    56465

---
+++ Service/Entity/OrderEntity.cs
@@ -0,0 +1,35 @@
+﻿using IMS.Service;
+using IMS.Service.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Service.Entity
+{
+    /// <summary>
+    /// 订单实体类
+    /// </summary>
+    public class OrderEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Standard { get; set; }//规格
+        public decimal Price { get; set; }
+        public decimal RealityPrice { get; set; }
+        public long GoodsAreaId { get; set; }
+        public GoodsAreaEntity GoodsArea { get; set; }
+        public long GoodsTypeId { get; set; }
+        public GoodsTypeEntity GoodsType { get; set; }
+        public long GoodsSecondTypeId { get; set; }
+        public GoodsSecondTypeEntity GoodsSecondType { get; set; }
+        public bool IsPutaway { get; set; }//是否上架
+        public bool IsRecommend { get; set; }//是否推荐
+        public string Description { get; set; }
+        public long GoodsImgId { get; set; }
+        public GoodsImgEntity GoodsImg { get; set; }
+        public long Inventory { get; set; }
+        public long SaleNum { get; set; }
+    }
+}
