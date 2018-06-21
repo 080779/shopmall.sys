70497054cdb08cd1722bab696f0db3e87fe8d7f6
080779 <exceed295@qq.com>
2018-06-21 14:24:04 +0800

    234234

---
+++ Web/Areas/Admin/Models/Log/ListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using IMS.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static IMS.Common.Pagination;
+
+namespace IMS.Web.Areas.Admin.Models.Log
+{
+    public class ListViewModel
+    {
+        public AdminLogDTO[] AdminLogs { get; set; }
+        public string PageHtml { get; set; }
+        public List<Page> Pages { get; set; }
+        public int PageCount { get; set; }
+    }
+}
